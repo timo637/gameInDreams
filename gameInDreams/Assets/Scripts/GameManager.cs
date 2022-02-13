a1b8b74d866959e25cf5c1a27da69f018de85712
timo637 <93084535+timo637@users.noreply.github.com>
2022-02-13 16:32:42 +0100

    dodajanje game managerja

---
+++ gameInDreams/Assets/Scripts/GameManager.cs
@@ -0,0 +1,155 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject playerPrefab;
+    public Text levelText;
+    public Text livesText;
+
+    public GameObject panelMenu;
+    public GameObject panelPlay;
+    public GameObject panelLevelCompleted;
+    public GameObject panelGameOver;
+
+    public GameObject[] levels;
+
+    public static GameManager Instance { get; private set; }
+
+    public enum State { MENU, INIT, PLAY, LEVELCOMPLETED, LOADLEVEL, GAMEOVER };
+    State _state;
+    GameObject _currentLevel;
+    bool _isSwitchingState;
+
+    private int _level;
+
+    public int Level
+    {
+        get { return _level; }
+        set { _level = value;
+            levelText.text = "Level: " + _level;
+        }
+    }
+
+    private int _livesRemaining;
+
+    public int LivesRemaining
+    {
+        get { return _livesRemaining; }
+        set { _livesRemaining = value;
+            livesText.text = "Lives remaining: " + _livesRemaining;
+        }
+    }
+
+
+    public void PlayClicked()
+    {
+        SwitchState(State.INIT);
+    }
+
+    void Start()
+    {
+        Instance = this;
+        SwitchState(State.MENU);
+    }
+
+    public void SwitchState(State newState, float delay = 0)
+    {
+        StartCoroutine(SwitchDelay(newState, delay));
+    }
+
+    IEnumerator SwitchDelay(State newState, float delay)
+    {
+        _isSwitchingState = true;
+        yield return new WaitForSeconds(delay);
+        EndState();
+        _state = newState;
+        BeginState(newState);
+        _isSwitchingState = false;
+    }
+
+    void BeginState(State newState)
+    {
+        switch (newState)
+        {
+            case State.MENU:
+                panelMenu.SetActive(true);
+                break;
+            case State.INIT:
+                panelPlay.SetActive(true);
+                Level = 1;
+                LivesRemaining = 3;
+                Instantiate(playerPrefab);
+                SwitchState(State.LOADLEVEL);
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVELCOMPLETED:
+                panelLevelCompleted.SetActive(true);
+                break;
+            case State.LOADLEVEL:
+                if (Level >= levels.Length)
+                {
+                    SwitchState(State.GAMEOVER);
+                }
+                else
+                {
+                    _currentLevel = Instantiate(levels[Level]);
+                    SwitchState(State.PLAY);
+                }
+                break;
+            case State.GAMEOVER:
+                panelGameOver.SetActive(true);
+                break;
+        }
+    }
+
+    void Update()
+    {
+        switch (_state)
+        {
+            case State.MENU:
+                break;
+            case State.INIT:
+                break;
+            case State.PLAY:
+                if (LivesRemaining == 0)
+                {
+                    SwitchState(State.GAMEOVER);
+                }
+                break;
+            case State.LEVELCOMPLETED:
+                break;
+            case State.LOADLEVEL:
+                break;
+            case State.GAMEOVER:
+                break;
+        }
+    }
+
+    void EndState()
+    {
+        switch (_state)
+        {
+            case State.MENU:
+                panelMenu.SetActive(false);
+                break;
+            case State.INIT:
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVELCOMPLETED:
+                panelLevelCompleted.SetActive(false);
+                break;
+            case State.LOADLEVEL:
+                break;
+            case State.GAMEOVER:
+                panelPlay.SetActive(false);
+                panelGameOver.SetActive(false);
+                break;
+        }
+    }
+}
